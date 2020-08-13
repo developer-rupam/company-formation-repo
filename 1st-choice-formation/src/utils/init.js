c4eb6c57e2334bcd4fc1b431ea3e3b5573459b00
rupamjana <rupam.jana@codomotive.com>
2020-08-13 19:43:04 +0530

    1st choice formation setup

---
+++ 1st-choice-formation/src/utils/init.js
@@ -0,0 +1,18 @@
+const DEVELOPMENTOBJ = {url : 'http://localhost:3000',api : 'http://localhost:8000'};
+const PRODUCTIONOBJ = {url : '',api : ''};
+
+export const mode = "dev" //dev : development; prod : production
+
+if(mode == 'dev'){
+    var url = DEVELOPMENTOBJ.url;
+    var api = DEVELOPMENTOBJ.api;
+}else{
+    var url = PRODUCTIONOBJ.url;
+    var api = PRODUCTIONOBJ.api;
+}
+
+export const ADDR = url;
+export const WEBSERVICE = api;
+
+export const SITENAME = '1st Choice Formation';
+export const SITENAMEALIAS = '1st_choice_formation';
