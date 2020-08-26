954d88b0e3e74d4e7516a7575c3f3f0d235e6df1
rupamjana <rupam.jana@codomotive.com>
2020-08-26 21:09:42 +0530

    baseurl (unsettled)

--- 1st-choice-formation/src/utils/init.js
+++ 1st-choice-formation/src/utils/init.js
@@ -1,5 +1,5 @@
 const DEVELOPMENTOBJ = {url : 'http://localhost:3000',api : 'http://127.0.0.1:6000/v1',baseurl : '/'};
-const PRODUCTIONOBJ = {url : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev ',api : 'http://127.0.0.1:6000/v1',baseurl : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev'};
+const PRODUCTIONOBJ = {url : 'https://hostingfordevelopment.000webhostapp.com/1st-choice-formation/dev ',api : 'http://127.0.0.1:6000/v1',baseurl : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev/'};
 
 export const mode = "prod" //dev : development; prod : production
 
--- smart-formation/src/utils/init.js
+++ smart-formation/src/utils/init.js
@@ -1,5 +1,5 @@
 const DEVELOPMENTOBJ = {url : 'http://localhost:3001',api : 'http://127.0.0.1:6000/v1',baseurl : '/'};
-const PRODUCTIONOBJ = {url : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev ',api : 'http://127.0.0.1:6000/v1',baseurl : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev'};
+const PRODUCTIONOBJ = {url : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev ',api : 'http://127.0.0.1:6000/v1',baseurl : 'https://hostingfordevelopment.000webhostapp.com/smart-formation/dev/'};
 
 export const mode = "prod" //dev : development; prod : production
 
